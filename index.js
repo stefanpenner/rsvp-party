7ef87c41a77f31c66027eb9c3d2f91c21b8a552c
Stefan Penner <stefan.penner@gmail.com>
2014-02-25 16:55:16 -0600

    add Promise.prototype.tap

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ Promise.prototype.returns = function(value) {
   });
 };
 
+Promise.prototype.tap = function(method) {
+  return this.then(function(value){
+    method(value);
+    return value;
+  });
+};
+
 Promise.prototype.invoke = function(method) {
   var args = Array.prototype.slice(arguments, 1);
 
@@ -14,7 +21,6 @@ Promise.prototype.invoke = function(method) {
     return value[method].apply(value, args);
   }, undefined, 'invoke: ' + method + ' with: ' + args);
 };
-
 
 Promise.prototype.map = function(mapFn) {
   return this.then(function(values) {
