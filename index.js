e8e839e82ad3c020bfa4163d4d2769d20c2c81d9
Stefan Penner <stefan.penner@gmail.com>
2014-03-04 21:35:09 -0500

    add sequence

--- index.js
+++ index.js
@@ -70,4 +70,14 @@ Promise.prototype.set = function(property, value) {
   });
 };
 
+Promise.sequence = function(tasks) {
+  var length = tasks.length;
+  var current = Promise.resolve();
+  var results = new Array(length);
 
+  for (var i = 0; i < length; ++i) {
+    current = results[i] = current.then(tasks[i]);
+  }
+
+  return Promise.all(results);
+};
