5e74007883e5bb690c1b5aee3aa23fa53aac4302
miguelcobain <jmandrade0@gmail.com>
2014-05-21 18:07:37 +0100

    Added promise enabled while loop

--- index.js
+++ index.js
@@ -101,3 +101,26 @@ RSVP.hashSequence = function(tasks) {
 
   return RSVP.hash(results);
 };
+
+//Run a promise returning callable body while a
+//promise returning callable condition is true.
+//Returns a promise.
+RSVP.promiseWhile = function(condition, body) {
+  return new RSVP.Promise(function(resolve,reject){
+  
+    function loop() {
+      RSVP.Promise.resolve(condition()).then(function(result){
+        // When the result of calling `condition` is no longer true, we are done.
+        if (!result){
+          resolve();
+        } else {
+          // When it completes loop again otherwise, if it fails, reject
+          RSVP.Promise.resolve(body()).then(loop,reject);
+        }
+      });
+    }
+
+    // Start running the loop
+    loop();
+  });
+};
